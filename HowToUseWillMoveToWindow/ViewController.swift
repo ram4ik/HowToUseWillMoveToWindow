606bef20b3dc9a5e2b8bd72f94a3568d78891338
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-07 14:48:48 +0300

    Initial Commit

---
+++ HowToUseWillMoveToWindow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HowToUseWillMoveToWindow
+//
+//  Created by ramil on 07/10/2019.
+//  Copyright © 2019 com.ri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
